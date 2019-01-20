4a2364243a78f3fb9a2ab38707bb5db030b2ca03
javi <xescape@live.ca>
2019-01-20 13:13:15 -0500

    gonna ask for 3 hours

--- NNRunner.py
+++ NNRunner.py
@@ -33,7 +33,7 @@ def getParams():
 
 def getHeader(name, out_path, err_path):
     
-    h = ["#!/bin/bash", "#SBATCH --nodes=1", "#SBATCH --ntasks-per-node=40", "#SBATCH --time=2:00:00", "#SBATCH --job-name {0}".format(name), \
+    h = ["#!/bin/bash", "#SBATCH --nodes=1", "#SBATCH --ntasks-per-node=40", "#SBATCH --time=3:00:00", "#SBATCH --job-name {0}".format(name), \
          "#SBATCH --output={0}".format(out_path), "#SBATCH --error={0}".format(err_path), ""] #extra line at the end
     
     return "\n".join(h)
