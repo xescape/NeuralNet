9f2946029c43a82d68bc677eeced568ee6fe250a
javi <javi.zhang@mail.utoronto.ca>
2019-12-30 17:57:53 -0500

    updating for auto running on scinet

--- KerasNet.py
+++ KerasNet.py
@@ -1,21 +1,82 @@
-import sys
+import sys, os, csv, logging, argparse
 import pandas as pd
 import sklearn as skl
 import tensorflow as tf
 import numpy as np
-import datetime
 from tensorflow.keras import layers, optimizers, utils, regularizers, backend as K
-from sklearn.preprocessing import OneHotEncoder as ohe, normalize
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LassoCV
+from sklearn.linear_model import LassoLarsCV
 from pathlib import Path
-from math import sqrt, pow
 from sklearn.model_selection import KFold
-import resnet
-import random
+
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+def run(df, meta_df, out_path, params):
+
+    print('Input shape is {0}'.format(str(df.shape)))
+    logger = logging.getLogger('main')
+    logger.info('New run with params layers: {0}, dropout {1}'.format(str(params['layers']), params['dropout']))
+
+    data = df.to_numpy()
+    meta = meta_df.to_numpy()
+
+    #scale data to [0, 1]
+    mms = MinMaxScaler()
+    data = mms.fit_transform(data)
+        
+    #prefilter
+    #try to reuse prefiltering results, but will redo if it didn't work
+    prefilter_path = out_path / 'prefilter.txt'
+    try:
+        with open(prefilter_path, 'r') as f:
+            reader = csv.reader(f, dialect = 'excel-tab', quoting=csv.QUOTE_NONNUMERIC)
+            data_idx = list(map(int, next(reader)))
+            # print(data_idx)
+            filtered_data = data[:, data_idx]
+        print('existing features used')
+    except:
+        print('no existing features, prefiltering')
+        n_f = 256
+        data_idx, filtered_data = prefiltering(data, meta, n_f)
+        with open(prefilter_path, 'w', newline = '') as f:
+            writer = csv.writer(f, dialect = 'excel-tab')
+            writer.writerow(data_idx)
+
+    logger.info('filtered data shape is ' + str(filtered_data.shape))
+
+    n_folds = 5
+    kf = KFold(n_splits=n_folds)
+    out_path = in_path / 'results'
+    out_path.mkdir(parents=True, exist_ok=True)
+    
+    for train_index, test_index in kf.split(data):
+
+        f_data_train, f_data_test = filtered_data[train_index], filtered_data[test_index]
+        # data_train, data_test = data[train_index], data[test_index]
+        meta_train, meta_test = meta[train_index], meta[test_index]
+
+        # print('lasso filtered')
+        # lasso_filtered_results, sorted_idx = trainLasso(f_data_train, f_data_test, meta_train, meta_test)
+        # write_result(meta_test, lasso_filtered_results, out_path / 'lasso_filtered.tsv')
+        
+        # print('lasso not filtered')
+        # lasso_results, sorted_idx = trainLasso(data_train, data_test, meta_train, meta_test)
+        # write_result(meta_test, lasso_results, out_path / 'lasso_nofilter.tsv')
+        
+        # print('dense filtered')
+        prefilter_results = trainPrefilterModel(f_data_train, f_data_test, meta_train, meta_test, params)
+        write_result(meta_test, prefilter_results, out_path / 'dense_filtered.tsv')
+
+        # print('dense not filtered')
+        # nofilter_results = trainPrefilterModel(data_train, data_test, meta_train, meta_test, log_path, model_path)
+        # write_result(meta_test, nofilter_results, out_path / 'dense_nofilter.tsv')
+    
+    print('end of run')
 
 def trainLasso(data_train, data_test, meta_train, meta_test):
-    model = LassoCV(cv = 5)
+    model = LassoLarsCV(cv = 5, n_jobs = -1)
+    
     model.fit(data_train, meta_train.reshape((meta_train.shape[0])))
     print('lasso model r2 = {0}'.format(model.score(data_test, meta_test.reshape((meta_test.shape[0])))))
 
@@ -24,120 +85,49 @@ def trainLasso(data_train, data_test, meta_train, meta_test):
 
     return model.predict(data_test), sorted_idx
 
-def trainFakeLasso(data, meta):
-    model = LassoCV(cv = 5)
-    model.fit(data, meta.reshape((meta.shape[0])))
-    print('lasso model r2 = {0}'.format(model.score(data, meta.reshape((meta.shape[0])))))
-    coefs = np.abs(model.coef_)
-    sorted_idx = sorted(range(coefs.shape[0]), key = lambda x: coefs[x], reverse = True)
-
-    return sorted_idx
-
 def prefiltering(data, meta, n):
     #make a linear model and get rid of the bottom stuff
-    model = LassoCV(cv = 3)
-    model.fit(data, meta.reshape((meta.shape[0])))
+    model = LassoLarsCV(n_jobs=-1)
+    model.fit(data, meta.reshape((meta.shape[0],)))
     coefs = np.abs(model.coef_)
     # coefs = coefs.reshape((coefs.shape[1],))
-    cutoff = max(sorted(coefs)[n], 0.000000000001) #minimum coefficient
+    # cutoff = max(sorted(coefs)[n], 0.00000000000000000000001) #minimum coefficient
 
-    good_idx = np.where((coefs >= cutoff))[0]
+    good_idx = np.where(coefs > 0)[0]
 
     res = data[:,good_idx]
 
-    print('{0} features selected'.format(len(good_idx)))
+    print('{0} features selected through prefiltering'.format(len(good_idx)))
+    return good_idx, res
+
+def makePrefilterModel(input_dim, neurons , dropout):
+    '''
+    this is the main function to make the model. start small
+    '''
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(input_dim, input_dim=input_dim, activation='relu', kernel_initializer='normal'))
+
+    for l in neurons:
+        model.add(layers.Dense(l, activation='relu', kernel_initializer='normal'))
+        model.add(layers.Dropout(dropout))
+    model.add(layers.Dense(1, kernel_initializer='normal'))
+
+    model.compile(optimizer=optimizers.Nadam(),
+                loss = 'mse',
+                metrics=['mae'])
+
+    return model
+
+def trainPrefilterModel(data_train, data_test, meta_train, meta_test, params):
+    input_dim = data_train.shape[1]
+    model = makePrefilterModel(input_dim, params['layers'], params['dropout'])
+
+    model.fit(data_train, meta_train, epochs=1500, batch_size=input_dim, shuffle=True, validation_split=0.25, verbose=0) #no tensorboard
+
+    res = model.evaluate(data_test, meta_test)
     
-    return res, good_idx
-
-
-def makeModel(input_dim):
-    '''
-    this is the main function to make the model. start small
-    '''
-    model = tf.keras.Sequential()
-    model.add(layers.Dense(input_dim, input_dim=input_dim, activation='relu', kernel_initializer='normal'))
-    model.add(layers.Dense(64, activation='relu', kernel_initializer='normal'))
-    model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(32, activation='relu', kernel_initializer='normal'))
-    model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(16, activation='relu', kernel_initializer='normal'))
-    model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(1, kernel_initializer='normal'))
-
-    model.compile(optimizer=optimizers.Nadam(),
-                loss = 'mse',
-                metrics=['mae'])
-
-    return model
-
-def trainModel(data_train, data_test, meta_train, meta_test):
-    input_dim = data_train.shape[1]
-    model = makeModel(input_dim)
-    model.fit(data_train, meta_train, epochs=3000, batch_size=5, shuffle=True, verbose=0)
-    print('base model training loss')
-    model.fit(data_train, meta_train, epochs=1, batch_size=5, shuffle=True, verbose=1)
-    print('base model eval')
-    model.evaluate(data_test, meta_test, batch_size=data_test.shape[1])
-    print('data mean = {0} and std = {1}'.format(np.mean(meta_test), np.std(meta_test)))
-
-def makePrefilterModel(input_dim):
-    '''
-    this is the main function to make the model. start small
-    '''
-    model = tf.keras.Sequential()
-
-    model.add(layers.Dense(input_dim, input_dim=input_dim, activation='relu', kernel_initializer='normal'))
-    model.add(layers.Dense(128, activation='relu', kernel_initializer='normal'))
-    model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(64, activation='relu', kernel_initializer='normal'))
-    model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(32, activation='relu', kernel_initializer='normal'))
-    model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(16, activation='relu', kernel_initializer='normal'))
-    model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(1, kernel_initializer='normal'))
-
-    model.compile(optimizer=optimizers.Nadam(),
-                loss = 'mse',
-                metrics=['mae'])
-
-    return model
-
-def makeSimplePrefilterModel(input_dim):
-    '''
-    this is the main function to make the model. start small
-    '''
-    model = tf.keras.Sequential()
-
-    model.add(layers.Dense(input_dim, input_dim=input_dim, activation='relu', kernel_initializer='normal'))
-    # model.add(layers.Dense(128, activation='relu', kernel_initializer='normal'))
-    # model.add(layers.Dropout(0.5))
-    # model.add(layers.Dense(64, activation='relu', kernel_initializer='normal'))
-    # model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(32, activation='relu', kernel_initializer='normal'))
-    # model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(16, activation='relu', kernel_initializer='normal'))
-    # model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(1, kernel_initializer='normal'))
-
-    model.compile(optimizer=optimizers.Nadam(),
-                loss = 'mse',
-                metrics=['mae'])
-
-    return model
-
-def trainPrefilterModel(data_train, data_test, meta_train, meta_test, log_path, model_path):
-    input_dim = data_train.shape[1]
-    model = makePrefilterModel(input_dim)
-    # tensorboard = tf.keras.callbacks.TensorBoard(log_dir=str(log_path))
-    model.fit(data_train, meta_train, epochs=2000, batch_size=input_dim, shuffle=True, validation_split=0.25, verbose=0) #no tensorboard
-    print('prefilter model training loss')
-    model.fit(data_train, meta_train, epochs=1, batch_size=20, shuffle=True, verbose=1)
-    print('prefilter model eval')
-    model.evaluate(data_test, meta_test, batch_size=data_test.shape[1])
-    print('data mean = {0} and std = {1}'.format(np.mean(meta_test), np.std(meta_test)))
-    # model.save(model_path)
-
+    logger = logging.getLogger('main')
+    logger.info('Loss of {2} in data mean = {0} and std = {1}'.format(np.mean(meta_test), np.std(meta_test), str(res)))
     return model.predict(data_test)
 
 def importData(paintings_path, meta_path):
@@ -147,66 +137,14 @@ def importData(paintings_path, meta_path):
     #for the paintings
     # if alt:
     #     df = read
-    df = pd.read_csv(paintings_path, sep='\t', header=None, index_col=0)
-    
+    df = pd.read_csv(paintings_path, sep='\t', header=0, index_col=0)
+
     #for the meta
     meta_df = pd.read_csv(meta_path, sep='\t', header=0, index_col=0)
     
     df, meta_df = df.align(meta_df, axis=0, join='inner')
     
     return df, meta_df
-
-
-def run(df, meta_df, in_path, log_path, model_path, data_out_path):
-
-    print('Input shape is {0}'.format(str(df.shape)))
-
-    data = df.to_numpy()
-    meta = meta_df.to_numpy()
-        
-    #prefilter
-    #try to reuse prefiltering results, but will redo if it didn't work
-    prefilter_path = in_path / 'prefilter.txt'
-    try:
-        with open(prefilter_path, 'r') as input:
-            data = input.read()
-        
-        lines = [(e[0], e[1]) for e in [f.split('\t') for f in data.strip('\n').split('\n')]]
-        filtered_data, data_idx = zip(*lines)
-    except:
-        n_f = 256
-        filtered_data, data_idx = prefiltering(data, meta, n_f)
-        with open(prefilter_path, 'w') as output:
-            lines = ['\t'.join(f, d) for f, d in zip(filtered_data, data_idx)]
-            output.write('\n'.join(lines))
-
-    n_folds = 5
-    kf = KFold(n_splits=n_folds)
-    out_path = in_path / 'results'
-    if not out_path.is_dir():
-        out_path.mkdir()
-
-    for train_index, test_index in kf.split(data):
-
-        f_data_train, f_data_test = filtered_data[train_index], filtered_data[test_index]
-        data_train, data_test = data[train_index], data[test_index]
-        meta_train, meta_test = meta[train_index], meta[test_index]
-
-        print('lasso filtered')
-        lasso_filtered_results, sorted_idx = trainLasso(f_data_train, f_data_test, meta_train, meta_test)
-        write_result(meta_test, lasso_filtered_results, out_path / 'lasso_filtered.tsv')
-        
-        print('lasso not filtered')
-        lasso_results, sorted_idx = trainLasso(data_train, data_test, meta_train, meta_test)
-        write_result(meta_test, lasso_results, out_path / 'lasso_nofilter.tsv')
-        
-        print('dense filtered')
-        prefilter_results = trainPrefilterModel(f_data_train, f_data_test, meta_train, meta_test, log_path, model_path)
-        write_result(meta_test, prefilter_results, out_path / 'dense_filtered.tsv')
-
-        print('dense not filtered')
-        nofilter_results = trainPrefilterModel(data_train, data_test, meta_train, meta_test, log_path, model_path)
-        write_result(meta_test, nofilter_results, out_path / 'dense_nofilter.tsv')
 
 def write_result(meta_test, result, out_path):
 
@@ -227,32 +165,82 @@ def write_result(meta_test, result, out_path):
         main_text = ['{0}\t{1}'.format(pair[0], pair[1]) for pair in zip(list(meta_test.reshape((-1,))), list(result.reshape((-1,))))]
         output.write('\n'.join(main_text))
 
+def configLogger(_path):
+    
+    logger = logging.getLogger('main')
+    logger.setLevel(logging.INFO)
+    
+    formatter = logging.Formatter('%(asctime)s:\n%(message)s\n')
+    
+    fh = logging.FileHandler(_path)
+    fh.setLevel(logging.INFO)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    
+    sh = logging.StreamHandler()
+    sh.setLevel(logging.INFO)
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+    
+    return logger
+
 if __name__ == "__main__":
 
+    def warn(*args, **kwargs):
+        pass
+    import warnings
+    warnings.warn = warn
+
+    #default paths
+    default_local_input_path = Path('D:\\Documents\\data\\plasmo\\nn_scalar')
+    default_local_output_path = default_local_input_path / 'output'
+    default_scinet_input_path = Path('/home/xescape/scratch/nn_scalar')
+    default_scinet_output_path = default_scinet_input_path / 'output'
+    default_layers = [128, 256, 64, 32, 16]
+    default_dropout = 0.3
+
+    #parse args
+    parser = argparse.ArgumentParser(description = 'Runs the NN model using Keras')
+    parser.add_argument('--local', action = 'store_true', default = False, dest = 'local')
+    parser.add_argument('-i', '--input', action = 'store', dest = 'input')
+    parser.add_argument('-o', '--output', action = 'store', dest = 'output')
+    parser.add_argument('-l', '--layers', action = 'store', nargs = "*", default = default_layers, type = int, dest = 'layers')
+    parser.add_argument('-d', '--dropout', action = 'store', default = default_dropout, type = float, dest = 'dropout')
     #for the new transformed data
     #defaults to running on scinet beluge in case I forget
-    in_path = Path('/home/xescape/scratch/nn_scalar')
-    try:
-        if sys.argv[1] == 'local':
-            in_path = Path('D:\\Documents\\data\\plasmo\\nn_scalar')
-    except:
-        continue
+
+    args = vars(parser.parse_args())
+
+    if args['input']:
+        in_path = Path(args['input'])
+    elif args['local'] == True :
+        in_path = default_local_input_path 
+    else:
+        in_path = default_scinet_input_path
+    
+    if args['output']:
+        out_path = Path(args['output'])
+    elif args['local'] == True :
+        out_path = default_local_output_path 
+    else:
+        out_path = default_scinet_output_path
+    
+    params = {
+        'layers': args['layers'],
+        'dropout': args['dropout']
+    }
 
     data_path = in_path / 'combined_scores.tsv'
     meta_path = in_path / 'meta.tsv'
-    log_dir = in_path / 'nn_logs'
+    
 
     #make the log folder if it's not there
-    if not log_dir.is_dir():
-        log_dir.mkdir()
-
-    log_path = in_path / 'nn_logs' / 'logs'
-    model_path = in_path / 'nn_logs' / 'curr_model5.h5'
-    data_out_path = in_path / 'nn_logs' / 'curr_data5.npz'
-
+    out_path.mkdir(parents=True, exist_ok=True)
+    log_path = out_path / 'logs.txt'
+    configLogger(log_path)
 
     df, meta_df = importData(data_path, meta_path)
-    run(df, meta_df, in_path, log_path, model_path, data_out_path)
+    run(df, meta_df, out_path, params)
 
     print('end!')
 
---
+++ KerasRunner.py
@@ -0,0 +1,120 @@
+'''
+Created on Jan. 17, 2019
+
+@author: Javi
+'''
+
+import pathlib
+import os
+import subprocess
+import re 
+import configparser
+import sys
+import timeit
+
+
+def getParams():
+    '''returns a dict, which contains parameters that we're changing. also includes a name
+        edit this one in the future!'''
+    
+    res = []
+
+    layers = [
+        [256, 128, 64, 32, 16],
+        [512, 256, 128, 64, 32, 16],
+        [64, 128, 128, 32, 16],
+        [64, 64, 64],
+        [64, 32, 16],
+        [64, 16],
+        [512, 64],
+    ]
+
+    dropout = [0.3, 0.5, 0.8]
+
+    for l in layers:
+        for d in dropout:
+            res.append((l, d))
+  
+    return res
+
+def getHeader(name, out_path, err_path):
+    
+    h = ["#!/bin/bash", "#SBATCH --nodes=1", "#SBATCH --ntasks-per-node=40", "#SBATCH --time=2:00:00", "#SBATCH --job-name {0}".format(name), \
+         "#SBATCH --output={0}".format(out_path), "#SBATCH --error={0}".format(err_path), ""] #extra line at the end
+    
+    return "\n".join(h)
+
+def getMods():
+    
+    m = ['module load anaconda3/5.1.0', 'source activate tensorflow', '']
+    
+    return "\n".join(m)
+
+
+def makeConfig(params):
+    '''create the string for config file'''
+    
+    c = ['{0}={1}'.format(key, params[key]) for key in params if key != 'name']
+
+    return '\n'.join(c)
+
+
+
+def makeScriptAndRun(script_path, root, p, mode):
+    '''make the config files as well as the sh script for job submission, then submit it
+    p[0] is a list of layers, p[1] is the dropout value'''
+    print(p)
+    name = 'L{0}D{1}'.format('_'.join([str(x) for x in p[0]]), str(p[1]))
+    folder = root / name
+    out_path = folder / '{0}_out.txt'.format(name)
+    err_path = folder / '{0}_err.txt'.format(name)
+    sh_path = folder / 'run_{0}.sh'.format(name)
+    
+    folder.mkdir(parents=True, exist_ok=True)
+
+    #creates and submits the job
+    if mode == 'local':
+        command = "python3 {script} -i {input} -o {output} -l {layers} -d {dropout} --local".format(script = script_path, input = root, output = folder, layers = ' '.join([str(x) for x in p[0]]), dropout = p[1])
+        sh_str = [getHeader(name, out_path, err_path), command]
+        with open(sh_path, 'w') as sh:
+            sh.write('\n'.join(sh_str))
+        subprocess.call(['bash', sh_path])
+    elif mode == 'scinet':
+        command = "python3 {script} -i {input} -o {output} -l {layers} -d {dropout}".format(script = script_path, input = root, output = folder, layers = ' '.join([str(x) for x in p[0]]), dropout = p[1])
+        sh_str = [getHeader(name, out_path, err_path), getMods(), command]
+        with open(sh_path, 'w') as sh:
+            sh.write('\n'.join(sh_str))       
+        subprocess.call(['sbatch', sh_path])
+        
+    print('job {0} submitted'.format(name))
+
+
+def main(script, root, mode):
+    '''the stuff'''
+
+    params = getParams()
+    
+    #we want to make a script for each param set
+    for p in params[:1]:
+        makeScriptAndRun(script, root, p, mode)
+
+
+if __name__ == '__main__':
+    
+    mode = sys.argv[1]
+    
+    if mode == 'local':
+        script = '/d/workspace/NeuralNet/KerasNet.py' #location of the nn script itself
+        root = '/d/data/plasmo/nn_scalar' #the root working directory. all jobs will be a separate directory under here. 
+    elif mode == 'scinet':
+        script = '/home/j/jparkin/xescape/NeuralNet/KerasNet.py'
+        root = '/scratch/j/jparkin/xescape/nn_scalar'
+    else:
+        raise(Exception('hey you didnt specify a mode'))
+    
+    script = pathlib.Path(script)
+    root = pathlib.Path(root)
+
+    
+    main(script, root, mode)
+    
